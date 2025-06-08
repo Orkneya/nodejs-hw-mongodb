d1be171e1f39c98764c521cd46a0720bb7909671
Tetyana Popenko <orkneya17@gmail.com>
2025-06-08 23:38:05 +0300

    hope

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -10,14 +10,6 @@ export const initMongoConnection = async () => {
     await mongoose.connect(
       `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`,
     );
-    // console.log('Подключаемся к MongoDB со следующими данными:');
-    // console.log({
-    //   user,
-    //   pwd,
-    //   url,
-    //   db,
-    //   fullConnectionString: `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`,
-    // });
     console.log('Mongo connection successfully established!');
   } catch (err) {
     console.log('Error while setting up mongo connection', err);
