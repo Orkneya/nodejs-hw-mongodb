58a283fc1dd1fb08b807e8167f2d06b5dc74f214
Tetyana Popenko <orkneya17@gmail.com>
2025-06-09 23:52:11 +0300

    field

--- src/server.js
+++ src/server.js
@@ -34,6 +34,8 @@ export const setupServer = () => {
       return;
     }
     res.status(200).json({
+      status: 200,
+      message: 'Successfully found contact with id {contactId}!',
       data: contact,
     });
   });
