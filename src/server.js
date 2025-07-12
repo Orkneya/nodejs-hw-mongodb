a43384e6636c0190b343168a25f48648eaf9a630
Tetyana Popenko <orkneya17@gmail.com>
2025-07-12 21:43:20 +0300

    swagger

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './db/models/swaggerDocs.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 export const setupServer = () => {
@@ -23,6 +24,8 @@ export const setupServer = () => {
   );
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+
+  app.use('/api-docs', swaggerDocs());
 
   app.use(router);
 
